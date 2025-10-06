8c62e393fb65519e4f705c8421a995ab3dbb7167
Cartersod <cartersoderena@gmail.com>
2025-10-05 21:31:04 -0400

    Final Adjustment
    10/5/25 9:30pm

--- Assets/RoomLightTrigger.cs
+++ Assets/RoomLightTrigger.cs
@@ -1,14 +1,17 @@
+using TMPro;
 using UnityEngine;
 
 public class RoomLightTrigger : MonoBehaviour
 {
     public Light directionalLight;
+    public TextMeshPro TextMeshPro;
 
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player")) // or your VR camera/player rig tag
         {
             directionalLight.enabled = true;
+            TextMeshPro.enabled = true;
         }
     }
 
@@ -17,6 +20,7 @@ public class RoomLightTrigger : MonoBehaviour
         if (other.CompareTag("Player"))
         {
             directionalLight.enabled = false;
+            TextMeshPro.enabled = false;
         }
     }
 }
